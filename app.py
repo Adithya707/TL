b296a334a90a001ab3ec6be6ff5c88d226d88438
Adithya707 <adithyach17@gamil.com>
2021-05-11 14:47:19 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask,render_template,request
-from tensorflow.keras.preprocessing.image import load_img
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import decode_predictions
-from tensorflow.keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+#from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
+from tensorflow import keras
+import json
 
 app = Flask(__name__)
-
+#model = VGG16()
+model = keras.models.load_model('model.h5')
 
 @app.route('/',methods=['GET'])
 def hello_world():
@@ -14,7 +17,7 @@ def hello_world():
 
 @app.route('/',methods=['POST'])
 def predict():
-    model = VGG16()
+    
     imagefile = request.files['imagefile']
     image_path = "./images/"+imagefile.filename
     imagefile.save(image_path)
@@ -28,6 +31,21 @@ def predict():
 
     classification = '%s (%.2f%%)' % (label[1],label[2]*100)
     return render_template('index.html',prediction=classification)
+
+def decode_predictions(preds, top=4, class_list_path='index.json'):
+    if len(preds.shape) != 2 or preds.shape[1] != 2: # your classes number
+         raise ValueError('`decode_predictions` expects '
+                     'a batch of predictions '
+                     '(i.e. a 2D array of shape (samples, 1000)). '
+                     'Found array with shape: ' + str(preds.shape))
+    index_list = json.load(open(class_list_path))
+    results = []
+    for pred in preds:
+       top_indices = pred.argsort()[-top:][::-1]
+       result = [tuple(index_list[str(i)]) + (pred[i],) for i in top_indices]
+       result.sort(key=lambda x: x[2], reverse=True)
+       results.append(result)
+    return results
 
 
 
